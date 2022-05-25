88a1c01a9c3eefe410c22d977dc4d6201254e2cf
josephlu2 <jjl55@duke.edu>
2022-05-24 20:10:30 -0400

    Initial Commit

---
+++ AlcTrack/AlcTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlcTrackApp.swift
+//  AlcTrack
+//
+//  Created by Joseph Lu on 5/24/22.
+//
+
+import SwiftUI
+
+@main
+struct AlcTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlcTrack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlcTrack
+//
+//  Created by Joseph Lu on 5/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
